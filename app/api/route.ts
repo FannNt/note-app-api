87c4d8b5a325b1cf93c2481c855c8679548f489e
fn <affanhelmi111@gmail.com>
2025-12-02 11:29:49 +0700

    add zod for validation and change response using nextResponse

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,15 +1,29 @@
 import {prisma} from "@/db";
+import {z} from "zod";
+import {NextResponse} from "next/server";
 
 export async function GET() {
     const notes = await prisma.note.findMany({})
 
-    return new Response(JSON.stringify(notes), {
+    return NextResponse.json(notes, {
         status: 200
     })
 }
 
-export async function POST(req:Request) {
-    const { name,message }= await req.json()
+
+export async function POST(req: Request) {
+    const Note = z.object({
+        name: z.string(),
+        message: z.string().min(5),
+    });
+
+    const body = await req.json();
+    const data = Note.safeParse(body)
+    if (!data.success) {
+        const error = z.flattenError(data.error)
+        return NextResponse.json(error.fieldErrors, {status: 400})
+    }
+    const {message, name} = data.data
 
     const note = await prisma.note.create({
         data: {
@@ -18,7 +32,7 @@ export async function POST(req:Request) {
         }
     })
 
-    return new Response(JSON.stringify(note), {
+    return NextResponse.json(note, {
         status: 201
     })
 
