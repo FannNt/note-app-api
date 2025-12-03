63da0c71d5be103bd847ee2dcbfa671eaf09f935
fn <affanhelmi111@gmail.com>
2025-12-03 14:18:20 +0700

    move register endpoint

--- app/api/user/register/route.ts
+++ app/api/user/register/route.ts
@@ -31,7 +31,7 @@ export async function POST(req: NextRequest) {
             },
         })
 
-        const token = createToken(user.email)
+        const token = createToken(user.id,user.email)
 
          return NextResponse.json({
              id: user.id,
